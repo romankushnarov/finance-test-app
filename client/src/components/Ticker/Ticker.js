5d00b83e77ae4ff7d7c8788672062a30ae64acb6
romankushnarov <kushnarov.roman@gmail.com>
2021-09-05 12:38:33 +0300

    arrows functional

--- client/src/components/Ticker/Ticker.js
+++ client/src/components/Ticker/Ticker.js
@@ -2,10 +2,11 @@ import styles from './Ticker.module.scss'
 import {usePrevious} from '../../hooks/usePreviousHook'
 
 export const Ticker = ({ticker, price, change, change_percent}) => {
-    const prevAmount = usePrevious({change, change_percent})
+    const prevAmount = usePrevious({price, change, change_percent})
 
     const oldChange = typeof prevAmount === 'undefined' ? change : prevAmount.change
     const oldChangePercent = typeof prevAmount === 'undefined' ? change_percent : prevAmount.change_percent
+    const oldPrice = typeof prevAmount === 'undefined' ? price : prevAmount.price
 
     let oldChangeClass = ''
     let changeSign = ''
@@ -22,15 +23,28 @@ export const Ticker = ({ticker, price, change, change_percent}) => {
     if(oldChangePercent > change_percent) {
         oldChangePercentClass = styles.red
         changePercentSign = '-'
-    } else if (oldChange < change) {
+    } else if (oldChangePercent < change_percent) {
         oldChangePercentClass = styles.green
         changePercentSign = '+'
     }
 
+    let rotateDeg = 270
+    let fillColor = 'black'
+    let bgColor = '#f8f9fa'
+    if(oldPrice < price) {
+        rotateDeg = 180
+        fillColor = 'green'
+        bgColor = '##e6f4ea'
+    } else if (oldPrice > price) {
+        rotateDeg = 0
+        fillColor = 'red'
+        bgColor = '#fce8e6'
+    }
+
     return (
         <div className={styles.ticker}>
-            <div className={styles['ticker_arrow']} style={{background: '#fce8e6'}}>
-                <svg width="16" height="16" viewBox="0 0 24 24" focusable="false" fill='red'>
+            <div className={styles['ticker_arrow']} style={{background: `${bgColor}`}}>
+                <svg width="16" height="16" viewBox="0 0 24 24" focusable="false" fill={fillColor} style={{transform: `rotate(${rotateDeg}deg)`}}>
                     <path d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z"></path>
                 </svg>
             </div>
