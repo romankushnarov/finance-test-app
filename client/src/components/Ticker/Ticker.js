e2d14b30f724934d619920d497e169f0077f0752
romankushnarov <kushnarov.roman@gmail.com>
2021-09-05 12:04:11 +0300

    main functional

--- client/src/components/Ticker/Ticker.js
+++ client/src/components/Ticker/Ticker.js
@@ -1,6 +1,32 @@
 import styles from './Ticker.module.scss'
+import {usePrevious} from '../../hooks/usePreviousHook'
 
 export const Ticker = ({ticker, price, change, change_percent}) => {
+    const prevAmount = usePrevious({change, change_percent})
+
+    const oldChange = typeof prevAmount === 'undefined' ? change : prevAmount.change
+    const oldChangePercent = typeof prevAmount === 'undefined' ? change_percent : prevAmount.change_percent
+
+    let oldChangeClass = ''
+    let changeSign = ''
+    if(oldChange > change) {
+        oldChangeClass = styles.red
+        changeSign = '-'
+    } else if (oldChange < change) {
+        oldChangeClass = styles.green
+        changeSign = '+'
+    }
+
+    let oldChangePercentClass = ''
+    let changePercentSign = ''
+    if(oldChangePercent > change_percent) {
+        oldChangePercentClass = styles.red
+        changePercentSign = '-'
+    } else if (oldChange < change) {
+        oldChangePercentClass = styles.green
+        changePercentSign = '+'
+    }
+
     return (
         <div className={styles.ticker}>
             <div className={styles['ticker_arrow']} style={{background: '#fce8e6'}}>
@@ -11,11 +37,11 @@ export const Ticker = ({ticker, price, change, change_percent}) => {
             <div className={styles['ticker_inner']}>
                 <div className={styles['ticker_row']}>
                     <p className={styles['ticker_name']}>{ticker}</p>
-                    <p className={styles['ticker_data-red']}>{change_percent} %</p>
+                    <p className={`${styles['ticker_data']} ${oldChangePercentClass}`}>{`${changePercentSign}${change_percent}`} %</p>
                 </div>
                 <div className={styles['ticker_row']}>
                     <p className={styles['ticker_price']}>{price}</p>
-                    <p className={styles['ticker_data-red']}>{change}</p>
+                    <p className={`${styles['ticker_data']} ${oldChangeClass}`}>{`${changeSign}${change}`}</p>
                 </div>
             </div>
         </div>
